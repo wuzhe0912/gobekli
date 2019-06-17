7213c957b3aaba98fb252910a9466969ae87967e
zhethon <kgb00128@gmail.com>
2019-06-17 08:41:15 +0800

    initial

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Hello extends React.Component {
+  render () {
+    const test = <h1> Hello React </h1>
+    console.log(test)
+    return (
+      <div>
+        <h1> Hello React </h1>
+      </div>
+    )
+  }
+}
+
+export default Hello
