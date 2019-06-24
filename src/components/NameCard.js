3fecd185d42155a5a91ed81bfdf23380868b69af
zhethon <kgb00128@gmail.com>
2019-06-24 23:26:45 +0800

    修改為函式寫法

--- src/components/NameCard.js
+++ src/components/NameCard.js
@@ -1,24 +1,22 @@
 import React from 'react'
 
-class NameCard extends React.Component {
-  render () {
-    const { name, number, isHuman, tags } = this.props
-    return (
-      <div className="alert alert-success">
-        <h4 className="alert-heading">{name}</h4>
-        <ul>
-          <li>電話：{number}</li>
-          <li>{ isHuman ? '地球人' : '火星人' }</li>
-          <hr/>
-          <p>
-            {tags.map((tag, index) =>(
-              <span className="badge badge-pill badge-primary" key={index}>{tag}</span>
-            ))}
-          </p>
-        </ul>
-      </div>
-    )
-  }
+const NameCard = (props) => {
+  const { name, number, isHuman, tags } = props
+  return (
+    <div className="alert alert-success">
+      <h4 className="alert-heading">{name}</h4>
+      <ul>
+        <li>電話：{number}</li>
+        <li>{ isHuman ? '地球人' : '火星人' }</li>
+        <hr/>
+        <p>
+          {tags.map((tag, index) =>(
+            <span className="badge badge-pill badge-primary" key={index}>{tag}</span>
+          ))}
+        </p>
+      </ul>
+    </div>
+  )
 }
 
 export default NameCard
