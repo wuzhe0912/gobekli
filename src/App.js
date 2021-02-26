963409423f8c0fcbef731d21edf66431ad450960
Pitt Wu <kgb00128@gmail.com>
2021-02-26 14:48:21 +0800

    feat：import sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+import React from "react";
+import "./styles/main.scss";
 
 function App() {
+  const list = [{ text: "aaa" }, { text: "bbb" }, { text: "ccc" }];
+
   return (
     <div className="App">
+      <span>{5 + 15}</span>
+      {list.map((node) => {
+        return <li key={node.text}>{node.text}</li>;
+      })}
     </div>
   );
 }
