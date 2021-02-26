1a3395affa39f8a96e4c69af76682f336487db14
Pitt Wu <kgb00128@gmail.com>
2021-02-26 15:39:31 +0800

    feat：避免直接 call function 寫法

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import "./styles/main.scss";
 
 function App() {
-  const handle = (e) => {
-    e.preventDefault();
-    let date = new Date().toLocaleDateString();
-    console.log(date);
+  const handle = (msg) => {
+    console.log(msg);
   };
 
   return (
     <div className="App">
-      <form action="">
-        <input type="text" />
-        <button onClick={handle}>Submit</button>
-      </form>
-      <p></p>
+      <button
+        onClick={() => {
+          handle("test");
+        }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
