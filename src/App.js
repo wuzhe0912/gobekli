3e58177f2d1655f73994b89ce261329a9433edb6
Pitt Wu <kgb00128@gmail.com>
2021-02-26 13:25:06 +0800

    feat：init new react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import NameCard from './components/NameCard'
-import LikesButton from './components/LikesButton'
-// const tags = ['尼安德塔人', '智人', '元謀人']
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      {/* <NameCard name="Pitt" number={12345} isHuman tags={tags} /> */}
-      <LikesButton />
     </div>
   );
 }
