791de9a628b8a92362e26d1a33ae72230c8e06b2
Pitt Wu <kgb00128@gmail.com>
2021-02-26 15:37:06 +0800

    feat：use onClick

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from "react";
-import Player from "./Player";
 import "./styles/main.scss";
 
 function App() {
-  const list = [
-    { name: "aaa", age: 11, desc: "lorem aaa" },
-    { name: "bbb", age: 22, desc: "lorem bbb" },
-    { name: "ccc", age: 33, desc: "lorem ccc" },
-  ];
+  const handle = (e) => {
+    e.preventDefault();
+    let date = new Date().toLocaleDateString();
+    console.log(date);
+  };
 
   return (
     <div className="App">
-      <span>{5 + 15}</span>
-      {list.map((node) => {
-        return (
-          <Player
-            key={node.name}
-            name={node.name}
-            age={node.age}
-            desc={node.desc}
-          />
-        );
-      })}
+      <form action="">
+        <input type="text" />
+        <button onClick={handle}>Submit</button>
+      </form>
+      <p></p>
     </div>
   );
 }
